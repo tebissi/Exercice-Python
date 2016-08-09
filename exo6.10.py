b5bfca466e957ce2bfcb064165fbd051183edce1
votre_pseudo <moi@email.com>
2016-08-09 07:23:33 +0200

    exo 6.10

---
+++ exo6.10.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.6
+# -*- coding : utf-8 -*
+# Homme ou femme
+
+prenom = raw_input("entrez votre prenom: ")
+nom = raw_input("entrez votre nom: ")
+sexe = raw_input("entrez f si vous etes une femme et m si vous etes un homme : ")
+
+while s != "f":
+ sexe = raw_input("la valeur entrez n'est ni f ni m : ")
+   if sexe == "f" or sexe =="m":
+     s  
+ print("bonjour Madame", prenom, nom)
+else:
+ print("bonjour Monsieur", prenom, nom)
+ 
+
